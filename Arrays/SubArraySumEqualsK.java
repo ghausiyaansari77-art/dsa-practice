26af36c31712a06befa2eb620ce1bd7f4c27a2c7
ghausiya Ansari <ghausiyaansari77@gmail.com>
2026-06-06 20:02:39 +0530

    Added array questions

---
+++ Arrays/SubArraySumEqualsK.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class SubArraySumEqualsk {
+   public int subarraySum(int[] nums, int k) {
+       int n = nums.length;
+
+
+       HashMap<Integer, Integer> map = new HashMap<>();
+       map.put(0, 1); // Initial prefix sum = 0 with 1 occurrence
+
+
+       int preSum = 0, ans = 0;
+
+
+       for (int i = 0; i < n; i++) {
+           preSum += nums[i];
+
+
+           // Check if (preSum - k) exists in the map
+           if (map.containsKey(preSum - k)) {
+               ans += map.get(preSum - k);
+           }
+
+
+           // Store prefix sum frequency
+           map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+       }
+       return ans;
+   }
+}
+
+
