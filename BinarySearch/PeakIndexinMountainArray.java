dedb9481af9afedac8942116197b764ee479c8ff
ghausiya Ansari <ghausiyaansari77@gmail.com>
2026-06-27 20:16:10 +0530

    Added String questions

---
+++ BinarySearch/PeakIndexinMountainArray.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+ class Solution {
+   public int peakIndexInMountainArray(int[] arr) {
+       int low = 0;
+       int high = arr.length - 1;
+      
+       while (low < high)
+       {
+           int mid = (low + high) / 2;
+           if (arr[mid] < arr[mid + 1]) {
+               low = mid + 1;
+           } else {
+               high = mid;
+           }
+       }
+       return low;
+   }
+}
+
+
