3511518cb598d38f6656a0046f2da0b4713034dc
ghausiya Ansari <ghausiyaansari77@gmail.com>
2026-07-14 17:51:27 +0530

    Added Linked List questions

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -1,32 +1,26 @@
 package LinkedList;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+
+
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-         // Initialize two pointers, slow and fast, to the head of the linked list.
-    ListNode slow = head;
-    ListNode fast = head;
-
-    // Move the slow pointer one step and the fast pointer two steps at a time through the linked list,
-    // until they either meet or the fast pointer reaches the end of the list.
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-      if (slow == fast) {
-        // If the pointers meet, there is a cycle in the linked list.
-        // Reset the slow pointer to the head of the linked list, and move both pointers one step at a time
-        // until they meet again. The node where they meet is the starting point of the cycle.
-        slow = head;
-        while (slow != fast) {
-          slow = slow.next;
-          fast = fast.next;
-          }
-        return slow;
-         }
-    }
-
-    // If the fast pointer reaches the end of the list without meeting the slow pointer,
-    // there is no cycle in the linked list. Return null.
-    return null;
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
         
     }
 }
